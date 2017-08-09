05b9992b9c10d3ec215b127d65970cad129bd5e7
张楠 <nan.zh@alibaba-inc.com>
2017-08-10 00:38:52 +0800

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const request = require('request');
+
+app.get('/', function (req, res) {
+	request({
+		url: "http://www.tuling123.com/openapi/api",
+		method: "POST",
+		json: true,
+		body: {"key":"7774b01eefe745ff8427cc7939e6affc", "info":req.query.word}
+	}, function (error, response, body) {
+		res.send({"success": true, "errorCode": "200", "errorMsg": "", "fields": {"回话": body.text}});
+	});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
