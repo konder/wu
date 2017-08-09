54481cbe5f8474d730ccc84595339775944de6be
zhangnan <konders@gmail.com>
2017-08-10 01:52:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', function (req, res) {
 		url: "http://www.tuling123.com/openapi/api",
 		method: "POST",
 		json: true,
-		body: {"key":"7774b01eefe745ff8427cc7939e6affc", "info":req.query.word}
+		body: {"key":"7774b01eefe745ff8427cc7939e6affc", "info":req.query.word.replace(/小美/, '')}
 	}, function (error, response, body) {
 		res.send({"success": true, "errorCode": "200", "errorMsg": "", "fields": {"回话": body.text}});
 	});
