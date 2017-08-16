8023bdb53315d480e493532e769f668980d69072
张楠 <nan.zh@alibaba-inc.com>
2017-08-16 12:25:36 +0800

    增加其它类支持

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ app.get('/', function (req, res) {
 		json: true,
 		body: {"key":"7774b01eefe745ff8427cc7939e6affc", "info":req.query.word.replace(/小美/, '')}
 	}, function (error, response, body) {
-		res.send({"success": true, "errorCode": "200", "errorMsg": "", "fields": {"回话": body.text}});
+		let word = "听不懂";
+                if (body.code == 100000){
+                        word = body.text;
+                }else if (body.code == 200000){
+                        word = body.url;
+                }else if (body.code == 302000){
+                        body.list.forEach(x=>{
+                                word += x.article + "(" + x.detailurl+ "),"
+                        })
+                }
+		res.send({"success": true, "errorCode": "200", "errorMsg": "", "fields": {"回话": word}});
 	});
 })
 
